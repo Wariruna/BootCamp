e5b2ca73021a41c499f70be9950ad971c3208f81
Wariruna <aaroncarretero@outlook.com>
2022-09-02 10:13:53 +0200

    extract Parts

---
+++ index.js
@@ -0,0 +1,35 @@
+[33mcommit 100a98f6842b4eaee4eb919f5bd5b2a2b47bd254[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Wariruna <aaroncarretero@outlook.com>
+Date:   Thu Sep 1 14:32:32 2022 +0200
+
+    Extraccion de props en h1
+
+[33mcommit dbc963ca78893027cc33cce2f4fbb25fc515f9bd[m
+Author: Wariruna <aaroncarretero@outlook.com>
+Date:   Thu Sep 1 14:24:55 2022 +0200
+
+    Exercise 1 finished
+
+[33mcommit 87e12c103d7ac27473540fd6c22a4aee62d6a179[m
+Author: Wariruna <aaroncarretero@outlook.com>
+Date:   Thu Sep 1 13:42:29 2022 +0200
+
+    se añade como lanzar react
+
+[33mcommit 2732e02849b96ebde11aa03849c99cbf8031da53[m
+Author: Wariruna <aaroncarretero@outlook.com>
+Date:   Thu Sep 1 13:21:23 2022 +0200
+
+    actualización
+
+[33mcommit 225bca9d92841b59352b8ef1f30461d77150aae2[m
+Author: Wariruna <aaroncarretero@outlook.com>
+Date:   Thu Sep 1 13:20:02 2022 +0200
+
+    subida de archivo como subir a github
+
+[33mcommit dde40c6a536f15daa016783cff38710d16ff0c49[m
+Author: Wariruna <aaroncarretero@outlook.com>
+Date:   Thu Sep 1 12:32:10 2022 +0200
+
+    Primer proyecto
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,22 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //Function type to one line(Return the first component)
-const Header = ({course}) => <h1>{course}</h1>
+const Header = ({ course }) => {
+
+  return (<h1>{course}</h1>)
+}
+
+//función one liner (Cuando una función solo devueve lo primero que está evaluando se puede omitir el return y las llaves).
+const Part1 = (props) => <p>{props.parts1} {props.exe}</p>
+//One liner function
+const Part2 = (props) => <p>{props.parts2} {props.exe2}</p>
+//ordinary component
+const Part3 = (props) => {
+  return (
+    <p>{props.parts3} {props.exe3}</p>
+  )
+}
 
 const Content = (props) => {
   return (
-    <p>{props.parts} {props.parts2}</p>
+    <div>
+      <Part1 parts1={props.parts1} exe={props.exe1}></Part1>
+      <Part2 parts2={props.parts2} exe2={props.exe2}></Part2>
+      <Part3 parts3={props.parts3} exe3={props.exe3}></Part3>
+    </div>
   )
 }
 
 const Total = (props) => {
-  return(
+  return (
     <p>Number of exercises {props.exe1 + props.exe2 + props.exe3} </p>
   )
 }
 
+
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -24,13 +43,13 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+
   return (
     <div>
-      <Header course={course} part={part1}></Header>
-      <Content parts={part1} parts2={exercises1}></Content>
-     <Content parts={part2} parts2={exercises2}></Content>
-     <Content parts={part3} parts2={exercises3}></Content>
-      <Total exe1={exercises1} exe2={exercises2} exe3={exercises3} />
+      <Header course={course}></Header>
+      <Content parts1={part1} exe1={exercises1} parts2={part2} exe2={exercises2} parts3={part3} exe3={exercises3}></Content>
+      <Total exe1={exercises1} exe2={exercises2} exe3={exercises3}></Total>
+
     </div>
   )
 }
